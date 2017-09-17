24e9ffeb22c8881e06e24fdd938389be3951aaa5
Blimmy59 <31900572+Blimmy59@users.noreply.github.com>
2017-09-16 23:29:08 -0700

    Added x y and z acceration queues, set baud rate to 128000.

--- Mech423Lab1/Form1.Designer.cs
+++ Mech423Lab1/Form1.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.Label label1;
-            System.Windows.Forms.Label label2;
-            System.Windows.Forms.Label label3;
-            System.Windows.Forms.Label label4;
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.textboxXvalue = new System.Windows.Forms.TextBox();
             this.connectDisconnect = new System.Windows.Forms.Button();
             this.comSelectionBox = new System.Windows.Forms.ComboBox();
@@ -41,51 +41,64 @@
             this.gumStick = new System.IO.Ports.SerialPort(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textboxSerialBufferSize = new System.Windows.Forms.TextBox();
-            label1 = new System.Windows.Forms.Label();
-            label2 = new System.Windows.Forms.Label();
-            label3 = new System.Windows.Forms.Label();
-            label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textboxQueueLength = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new System.Drawing.Point(121, 129);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(78, 25);
-            label1.TabIndex = 1;
-            label1.Text = "xValue";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "xValue";
             // 
             // label2
             // 
-            label2.AutoSize = true;
-            label2.Location = new System.Drawing.Point(121, 203);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(78, 25);
-            label2.TabIndex = 4;
-            label2.Text = "zValue";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "zValue";
             // 
             // label3
             // 
-            label3.AutoSize = true;
-            label3.Location = new System.Drawing.Point(121, 166);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(78, 25);
-            label3.TabIndex = 5;
-            label3.Text = "yValue";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "yValue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 48);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Serial Buffer Size";
             // 
             // textboxXvalue
             // 
-            this.textboxXvalue.Location = new System.Drawing.Point(205, 129);
+            this.textboxXvalue.Location = new System.Drawing.Point(102, 102);
+            this.textboxXvalue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textboxXvalue.Name = "textboxXvalue";
-            this.textboxXvalue.Size = new System.Drawing.Size(100, 31);
+            this.textboxXvalue.Size = new System.Drawing.Size(52, 20);
             this.textboxXvalue.TabIndex = 0;
             // 
             // connectDisconnect
             // 
-            this.connectDisconnect.Location = new System.Drawing.Point(162, 29);
+            this.connectDisconnect.Location = new System.Drawing.Point(81, 15);
+            this.connectDisconnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.connectDisconnect.Name = "connectDisconnect";
-            this.connectDisconnect.Size = new System.Drawing.Size(129, 40);
+            this.connectDisconnect.Size = new System.Drawing.Size(64, 21);
             this.connectDisconnect.TabIndex = 2;
             this.connectDisconnect.Text = "Connect";
             this.connectDisconnect.UseVisualStyleBackColor = true;
@@ -94,28 +107,32 @@
             // comSelectionBox
             // 
             this.comSelectionBox.FormattingEnabled = true;
-            this.comSelectionBox.Location = new System.Drawing.Point(35, 34);
+            this.comSelectionBox.Location = new System.Drawing.Point(18, 18);
+            this.comSelectionBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comSelectionBox.Name = "comSelectionBox";
-            this.comSelectionBox.Size = new System.Drawing.Size(121, 33);
+            this.comSelectionBox.Size = new System.Drawing.Size(62, 21);
             this.comSelectionBox.TabIndex = 3;
             this.comSelectionBox.DropDown += new System.EventHandler(this.comSelectionBox_DropDown);
             // 
             // textboxZvalue
             // 
-            this.textboxZvalue.Location = new System.Drawing.Point(205, 203);
+            this.textboxZvalue.Location = new System.Drawing.Point(102, 141);
+            this.textboxZvalue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textboxZvalue.Name = "textboxZvalue";
-            this.textboxZvalue.Size = new System.Drawing.Size(100, 31);
+            this.textboxZvalue.Size = new System.Drawing.Size(52, 20);
             this.textboxZvalue.TabIndex = 6;
             // 
             // textboxYvalue
             // 
-            this.textboxYvalue.Location = new System.Drawing.Point(205, 166);
+            this.textboxYvalue.Location = new System.Drawing.Point(102, 121);
+            this.textboxYvalue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textboxYvalue.Name = "textboxYvalue";
-            this.textboxYvalue.Size = new System.Drawing.Size(100, 31);
+            this.textboxYvalue.Size = new System.Drawing.Size(52, 20);
             this.textboxYvalue.TabIndex = 7;
             // 
             // gumStick
             // 
+            this.gumStick.BaudRate = 128000;
             this.gumStick.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.gumStick_DataReceived);
             // 
             // timer1
@@ -124,37 +141,50 @@
             this.timer1.Interval = 15;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new System.Drawing.Point(30, 92);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(178, 25);
-            label4.TabIndex = 9;
-            label4.Text = "Serial Buffer Size";
-            // 
             // textboxSerialBufferSize
             // 
-            this.textboxSerialBufferSize.Location = new System.Drawing.Point(205, 92);
+            this.textboxSerialBufferSize.Location = new System.Drawing.Point(102, 48);
+            this.textboxSerialBufferSize.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textboxSerialBufferSize.Name = "textboxSerialBufferSize";
-            this.textboxSerialBufferSize.Size = new System.Drawing.Size(100, 31);
+            this.textboxSerialBufferSize.Size = new System.Drawing.Size(52, 20);
             this.textboxSerialBufferSize.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 72);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Queue Length";
+            // 
+            // textboxQueueLength
+            // 
+            this.textboxQueueLength.Location = new System.Drawing.Point(102, 72);
+            this.textboxQueueLength.Margin = new System.Windows.Forms.Padding(2);
+            this.textboxQueueLength.Name = "textboxQueueLength";
+            this.textboxQueueLength.Size = new System.Drawing.Size(52, 20);
+            this.textboxQueueLength.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(518, 478);
-            this.Controls.Add(label4);
+            this.ClientSize = new System.Drawing.Size(259, 249);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textboxQueueLength);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textboxSerialBufferSize);
             this.Controls.Add(this.textboxYvalue);
             this.Controls.Add(this.textboxZvalue);
-            this.Controls.Add(label3);
-            this.Controls.Add(label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comSelectionBox);
             this.Controls.Add(this.connectDisconnect);
-            this.Controls.Add(label1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textboxXvalue);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -172,6 +202,12 @@
         private System.IO.Ports.SerialPort gumStick;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textboxSerialBufferSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textboxQueueLength;
     }
 }
 
--- Mech423Lab1/Form1.cs
+++ Mech423Lab1/Form1.cs
@@ -15,7 +15,9 @@ namespace WindowsFormsApp2
     public partial class Form1 : Form
     {
         ConcurrentQueue<int> dataQueue = new ConcurrentQueue<int>();
-
+        ConcurrentQueue<int> xData = new ConcurrentQueue<int>();
+        ConcurrentQueue<int> yData = new ConcurrentQueue<int>();
+        ConcurrentQueue<int> zData = new ConcurrentQueue<int>();
 
         public Form1()
         {
@@ -58,20 +60,57 @@ namespace WindowsFormsApp2
             {
                 int bufferSize = gumStick.BytesToRead;
                 textboxSerialBufferSize.Text = bufferSize.ToString();
+
+                int queueLength = dataQueue.Count();
+                textboxQueueLength.Text = queueLength.ToString();
+
+                int currentByte;
+                int dataByteState = 0;
+
+                while (dataQueue.Count > 0)
+                {
+                    if (dataQueue.TryDequeue(out currentByte) == true)
+                    {
+                        if ((currentByte == 255) && (dataByteState == 0))
+                        {
+                            dataByteState = 1;
+                        }
+
+                        else if (dataByteState == 1)
+                        {
+                            xData.Enqueue(currentByte);
+                            textboxXvalue.Text = currentByte.ToString();
+                            dataByteState = 2;
+                        }
+
+                        else if (dataByteState == 2)
+                        {
+                            yData.Enqueue(currentByte);
+                            textboxYvalue.Text = currentByte.ToString();
+                            dataByteState = 3;
+                        }
+
+                        else if (dataByteState == 3)
+                        {
+                            zData.Enqueue(currentByte);
+                            textboxZvalue.Text = currentByte.ToString();
+                            dataByteState = 0;
+                        }
+                    }
+                }
             }
         }
 
         private void gumStick_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
-            
-            
-            //int bufferSize = gumStick.BytesToRead;
+            int bufferSize = gumStick.BytesToRead;
 
-            //for (int i = 0; i < bufferSize; i++)
-            //{
-            //    int temp = gumStick.ReadByte();
-            //    dataQueue.Enqueue(temp);
-            //}
+            while (bufferSize > 4)
+            {
+                int temp = gumStick.ReadByte();
+                dataQueue.Enqueue(temp);
+                bufferSize--;
+            }
         }
     }
 }
